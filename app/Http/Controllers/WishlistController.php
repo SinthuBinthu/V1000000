49e9ada608499bdd20341147530fc8a7e6d13319
Sinthujan Sivakumaran <sinthujan.sivakumaran@edu.tbz.ch>
2025-05-05 20:25:52 +0200

    Push-Everything
    Public

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Surfsidemedia\Shoppingcart\Cart;
+use App\Models\Product;
+class WishlistController extends Controller
+{
+    public function add_to_wishlist(Request $request)
+    {
+        Cart::instance('wishlist')
+            ->add($request->id, $request->name, $request->quantity, $request->price)
+            ->associate(Product::class);
+
+        return back();
+}
+}
